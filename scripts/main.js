ded4a3af5b6b4808ac72c43ecb391c46b96e96a1
Hugo Kjellerstedt <hugo.kjellerstedt@gmail.com>
2022-03-31 16:49:06 +0200

    fixed ipad responsivenes for home, shop

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,15 @@
-let btn = document.querySelector("button");
-btn.addEventListener("click", function() {
-    alert("Tack för beställningen. Vi återkommer med tider då vi kan leverera produkten till dig bästa kund. Hoppas du gillar vår produkt.");
-});
+$(document).ready(() => {
+    let btn = $("button")
+    let menu = $("#hamburgermenu")
+    let nav = $("#mobileNav")
+
+    btn.click(() => {
+        alert("Tack för beställningen. Vi återkommer med tider då vi kan leverera produkten till dig bästa kund. Hoppas du gillar vår produkt.")
+    })
+
+    menu.click(() => {
+        nav.slideToggle()
+    })
+    
+})
+
